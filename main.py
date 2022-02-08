91e59f17b95ccabfd5973af955dacdf5cf161dc6
BogdanEgorov <61249711+BogdanEgorov@users.noreply.github.com>
2022-02-08 12:57:18 +0300

    Add files

---
+++ main.py
@@ -0,0 +1,11 @@
+my_height = 178
+my_weight = 65
+strides_count = 1000
+activity_time = 60
+
+stride = my_height / 4 + 0.37
+s = strides_count / activity_time
+v = s / activity_time
+consumption_energy = 0.035 * my_weight + (v**2 // my_height) * 0.029 * my_weight
+
+print("Пройденная дистанция: " + str(s) + " Количество сожженных калорий: " + str(consumption_energy))
