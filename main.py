456904f258e800be6bc48c14059d209dcc09ce70
BogdanEgorov <61249711+BogdanEgorov@users.noreply.github.com>
2022-02-08 13:19:52 +0300

    Добавили вывод комментария о количествах пройденных км

--- main.py
+++ main.py
@@ -8,5 +8,11 @@ s = strides_count / activity_time
 v = s / activity_time
 consumption_energy = 0.035 * my_weight + (v**2 // my_height) * 0.029 * my_weight
 distance = (v * activity_time) / 1000
+if (distance > 4):
+    print("Вы прошли больше 4 км. Так держать!")
+elif (distance > 2):
+    print("Вы прошли больше 2 км. Неплохо!")
+elif (distance < 2):
+    print("Вы прошли меньше 2 км. Уже устали?")
 
 print("Пройденная дистанция: " + str(s) + " Количество сожженных калорий: " + str(consumption_energy) + " Количество пройденных км: " + str(distance))
